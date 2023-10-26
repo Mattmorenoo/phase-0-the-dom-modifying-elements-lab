e1a5e72d9fb500a085d299cf7b16926096bba8e7
Matthew Moreno <Matthewm1398@gmail.com>
2023-10-26 16:30:47 -0700

    complete files

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const mainElement = document.querySelector("#main");
+mainElement.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set the id attribute of the new <h1> element to "victory"
+newHeader.id = "victory";
+
+// Set the text content of the new <h1> element to "YOUR-NAME is the champion"
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// Append the new <h1> element to the document body
+document.body.appendChild(newHeader);
